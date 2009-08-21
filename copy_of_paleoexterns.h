3e5d5a6843174a92807f6bfea308c0a5468b66be
Jeremy Archer <fatlotus@gmail.com>
2009-08-21 11:20:31 -0500

    Reorganized directory structure

--- copy_of_paleoexterns.h
+++
@@ -1,6 +0,0 @@
-extern ELEMENT 		*head_elem, *cur_elem, *tail_elem;
-extern CONTINENT	*cont_list[];
-extern int 		Next_Phigs_Struct;
-extern PLATE		*plate_list[];
-extern PMAG		pmag_list[2000];
-extern LITH		lith_list[5000];
